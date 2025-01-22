1455db31f8c84f2814ca8e34421148c5bf0bd197
AAAAlvin <bdeok35@gmail.com>
2025-01-22 23:18:20 +0900

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,30 +10,17 @@ import { MainTop } from './maintop';
 export default function Main() {
   return (
     <>
-      <Head>
-        <meta property="og:title" content="웨딩 초대장" />
-        <meta property="og:description" content="2025년 4월 26일, 유영현과 김아람의 결혼식에 초대합니다!" />
-        <meta property="og:image" content="https://example.com/images/og-image.jpg" />
-        <meta property="og:url" content="https://example.com" />
-        <meta property="og:type" content="website" />
-
-        <meta name="twitter:title" content="웨딩 초대장" />
-        <meta name="twitter:description" content="2025년 4월 26일, 유영현과 김아람의 결혼식에 초대합니다!" />
-        <meta name="twitter:image" content="https://example.com/images/twitter-image.jpg" />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
-      
-    <div>
-      <div className="flex flex-col  items-center min-h-screen bg-neutral-100"> {/* 화면 중앙에 배치 */}
-        <div className="relative w-[425px]">
-          <MainTop/>
-          <MainIntro />
-          <MainLocation/>
-          <MainAccount/>
-          {/* <MainTitle title="GALLERY" subtitle="우리의 순간"/> */}
+      <div>
+        <div className="flex flex-col  items-center min-h-screen bg-neutral-100"> {/* 화면 중앙에 배치 */}
+          <div className="relative w-[425px]">
+            <MainTop/>
+            <MainIntro />
+            <MainLocation/>
+            <MainAccount/>
+            {/* <MainTitle title="GALLERY" subtitle="우리의 순간"/> */}
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
