00404b650eff0562eb7b4d4abfbf35d15e66a136
AAAAlvin <bdeok35@gmail.com>
2025-01-22 23:04:13 +0900

    Meta 데이터 적용 샘플

---
+++ src/app/mainaccount.tsx
@@ -0,0 +1,25 @@
+import { MainTitle } from "./maintitle";
+import { Map } from "./map";
+
+export function MainAccount() {
+    return (
+      <div className="bg-white">
+        <div className="bg-white p-1">
+            <MainTitle title="ACCOUNT" subtitle="마음 전하실 곳"/>
+            <div className="row-03 p-6">
+            <div className="text-center text-gray-600 font-gowun">
+                <div className="p-3">
+                    참석이 어려우신 분들을 위해
+                    <br/>
+                    계좌번호를 기재하였습니다.
+                    <br/>
+                    너그러운 마음으로 양해 부탁드립니다.
+                </div>
+              </div>
+            </div>
+        </div>
+  
+      </div>
+    );
+  }
+  
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,43 @@
 // "use client";
 
 // import { useState } from "react";
+import Head from 'next/head';
+import { MainAccount } from './mainaccount';
 import { MainIntro } from './mainintro';
 import { MainLocation } from './mainlocation';
 import { MainTop } from './maintop';
 
 export default function Main() {
   return (
+    <>
+      <Head>
+        <meta property="og:title" content="웨딩 초대장" />
+        <meta property="og:description" content="2025년 4월 26일, 유영현과 김아람의 결혼식에 초대합니다!" />
+        <meta property="og:image" content="https://example.com/images/og-image.jpg" />
+        <meta property="og:url" content="https://example.com" />
+        <meta property="og:type" content="website" />
+
+        <meta name="twitter:title" content="웨딩 초대장" />
+        <meta name="twitter:description" content="2025년 4월 26일, 유영현과 김아람의 결혼식에 초대합니다!" />
+        <meta name="twitter:image" content="https://example.com/images/twitter-image.jpg" />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <div>
+        <h1>Welcome to the Wedding Page!</h1>
+      </div>
+      
     <div>
       <div className="flex flex-col  items-center min-h-screen bg-neutral-100"> {/* 화면 중앙에 배치 */}
         <div className="relative w-[425px]">
           <MainTop/>
           <MainIntro />
           <MainLocation/>
+          <MainAccount/>
           {/* <MainTitle title="GALLERY" subtitle="우리의 순간"/> */}
         </div>
       </div>
     </div>
+    </>
   );
 }
 
