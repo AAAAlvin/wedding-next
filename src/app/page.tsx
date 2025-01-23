d26bceda93a41876355e0a6a46527f546ef4a895
AAAAlvin <bdeok35@gmail.com>
2025-01-24 00:07:54 +0900

    meta 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,21 @@ import { MainIntro } from './mainintro';
 import { MainLocation } from './mainlocation';
 import { MainTop } from './maintop';
 import { Comment } from './maincomment';
+import Head from "next/head";  // 잘못된 임포트 -> next/document에서 next/head로 변경
 
 export default function Main() {
   return (
     <>
+      <Head>
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.wedding-yh-ahram.site/"/>
+        <meta property="og:title" content="영현♥아람 결혼합니다!" />
+        <meta property="og:description" content="영현♥아람 결혼합니다!" />
+      </Head>
+
       <div>
         <div className="flex flex-col items-center min-h-screen bg-neutral-100"> {/* 화면 중앙에 배치 */}
-          <div className="relative w-full">
+          <div className="relative">
             <MainTop/>
             <MainIntro />
             <MainLocation/>
