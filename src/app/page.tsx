2b7a926509b384bd828ffdefd0f504eb22ddb180
AAAAlvin <bdeok35@gmail.com>
2025-01-28 01:45:51 +0900

    메타 데이터 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { MainIntro } from './mainintro';
 import { MainLocation } from './mainlocation';
 import { MainTop } from './maintop';
 import { Comment } from './maincomment';
-import Head from "next/head";  // 잘못된 임포트 -> next/document에서 next/head로 변경
+// import Head from "next/head";  // 잘못된 임포트 -> next/document에서 next/head로 변경
 import { Bgm } from './bgm';
 
 // import Carousel from './carousel';
@@ -13,12 +13,12 @@ import { Bgm } from './bgm';
 export default function Main() {
   return (
     <>
-      <Head>
+      {/* <Head>
+      <meta property="og:url" content="https://www.wedding-yh-ahram.site/"/>
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://www.wedding-yh-ahram.site/"/>
         <meta property="og:title" content="영현♥아람 결혼합니다!" />
-        <meta property="og:description" content="4월 26일 (토) 오후 5시30분" />
-      </Head>
+        <meta property="og:description" content="4월 26일(토) 오후 5시30분" />
+      </Head> */}
 
       <div>
         <div className="flex flex-col items-center min-h-screen bg-neutral-100"> {/* 화면 중앙에 배치 */}
