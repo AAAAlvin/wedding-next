cec5ccc3ed3f232d47a0f60e465d8f4534143e18
AAAAlvin <bdeok35@gmail.com>
2025-01-30 11:59:45 +0900

    구글 맵 수정

--- src/app/map.tsx
+++ src/app/map.tsx
@@ -5,7 +5,7 @@ export function Map() {
       <div className="flex justify-center">
         <div className="relative w-4/5 pb-[56.25%]"> {/* 16:9 비율 유지 */}
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d3175.12361179637!2d126.99650397585998!3d37.26849577211727!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sko!2skr!4v1737547104288!5m2!1sko!2skr"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d943.9692335843314!2d126.99924383682101!3d37.268548733630205!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357b431e409a9307%3A0x1b2d431dea0a4da0!2z64W467O07YWUIOyVsOuwsOyEnOuNlCDsiJjsm5A!5e0!3m2!1sko!2skr!4v1738205913224!5m2!1sko!2skr"
             className="absolute top-0 left-0 w-full h-full"
             style={{ border: 0 }}
             allowFullScreen
