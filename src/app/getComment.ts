77a8fb2b6758cc10e2ecbcddf22c84f9c666450a
AAAAlvin <bdeok35@gmail.com>
2025-05-12 21:42:05 +0900

    코멘트 정렬순서 변경

--- src/app/getComment.ts
+++ src/app/getComment.ts
@@ -2,7 +2,7 @@ import { createClient } from '@/utils/supabase/server';
 
 export async function GetComment() {
   const supabase = await createClient();
-  const { data: varComments } = await supabase.from("comments").select('*');
+  const { data: varComments } = await supabase.from("comments").select('*').order('created_at', { ascending: false });
 
   // null을 빈 배열로 처리
   return varComments ?? [];
